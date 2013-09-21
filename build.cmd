3fc964adac52c3906311b0f18724f7aa3a47338a
utapyngo <ut@pyngo.tom.ru>
2013-09-21 19:43:38 +0700

    Echo off

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
-for /D %%d in (%SystemRoot%\Microsoft.NET\Framework64\v4*) do set "msbuild=%%d\MSBuild.exe"
-if not exist %msbuild% for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
-if not exist %msbuild% set "msbuild=%SystemRoot%\Microsoft.NET\Framework64\v3.5\MSBuild.exe"
-if not exist %msbuild% set "msbuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+@echo off
 
+for /D %%d in (%SystemRoot%\Microsoft.NET\Framework64\v4*) do set "msbuild=%%d\MSBuild.exe"
+if not exist "%msbuild%" for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
+if not exist "%msbuild%" set "msbuild=%SystemRoot%\Microsoft.NET\Framework64\v3.5\MSBuild.exe"
+if not exist "%msbuild%" set "msbuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+
+@echo on
 %msbuild% /p:Configuration=Release;DebugType=None;OutDir=..\
