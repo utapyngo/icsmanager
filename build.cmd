904fdbd9ecc87540d41e4021675881130561188c
utapyngo <ut@pyngo.tom.ru>
2013-09-21 20:16:29 +0700

    Not using Framework64 for building anymore

--- build.cmd
+++ build.cmd
@@ -1,8 +1,6 @@
 @echo off
 
-for /D %%d in (%SystemRoot%\Microsoft.NET\Framework64\v4*) do set "msbuild=%%d\MSBuild.exe"
-if not exist "%msbuild%" for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
-if not exist "%msbuild%" set "msbuild=%SystemRoot%\Microsoft.NET\Framework64\v3.5\MSBuild.exe"
+for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
 if not exist "%msbuild%" set "msbuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
 
 @echo on
