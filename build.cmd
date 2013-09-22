370487cb463c7408f9de7de2b7ba9de1913f235e
utapyngo <ut@pyngo.tom.ru>
2013-09-22 15:34:51 +0700

    Fixed build script

--- build.cmd
+++ build.cmd
@@ -1,7 +1,11 @@
 @echo off
 
 for /D %%d in (%SystemRoot%\Microsoft.NET\Framework\v4*) do set "msbuild=%%d\MSBuild.exe"
-if not exist "%msbuild%" set "msbuild=%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe"
+if not exist "%msbuild%" (
+	echo .NET Framework v4.0 is required to build this program
+	pause
+	goto :EOF
+)
 
 @echo on
 %msbuild% /p:Configuration=Release;DebugType=None;OutDir=..\
